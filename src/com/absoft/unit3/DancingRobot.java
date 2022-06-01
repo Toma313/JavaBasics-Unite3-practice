c6f01ad32e8c481083a16855e0dfad19d83fa6b5
tamara.voloshchuk <tamara.voloshchuk01@gmail.com>
2022-06-02 01:02:06 +0300

    add dancing robot

---
+++ src/com/absoft/unit3/DancingRobot.java
@@ -0,0 +1,14 @@
+package com.absoft.unit3;
+
+public  abstract class DancingRobot {
+    public String getDefaultDanceType1(){
+        return getDanceType1();
+    };
+
+
+    public abstract String getDanceType1();
+
+    public abstract String getDanceType2();
+
+}
+
---
+++ src/com/absoft/unit3/FolkDancingRobot.java
@@ -0,0 +1,13 @@
+package com.absoft.unit3;
+
+public class FolkDancingRobot extends DancingRobot{
+    @Override
+    public String getDanceType1() {
+        return "Oy y lizy";
+    }
+
+    @Override
+    public String getDanceType2() {
+        return "falk dance 2 ";
+    }
+}
